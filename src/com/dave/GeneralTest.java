b69fd9f7de5eb36f924f1a40999488ac85d7035a
DMoos <dmoos@datarecognitioncorp.com>
2012-12-06 08:48:09 -0600

    local added comment

--- src/com/dave/GeneralTest.java
+++ src/com/dave/GeneralTest.java
@@ -8,7 +8,7 @@ public class GeneralTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Say Hello localedit");
-		// I added this comment to my local git
+		// I added this comment to my local gita
 	}
 
 }
