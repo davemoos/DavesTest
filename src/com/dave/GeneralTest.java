c66c795245c3b1406399752a406fb1447abc1502
DMoos <dmoos@datarecognitioncorp.com>
2012-12-06 20:01:38 -0600

    change to local

--- src/com/dave/GeneralTest.java
+++ src/com/dave/GeneralTest.java
@@ -6,10 +6,6 @@ public class GeneralTest {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Say Hello localedit");
-		// I added this comment to my local gita
-		// merge line
 	}
-
 }
