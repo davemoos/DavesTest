f9b58d29bb670a03f006a6ff611ebea2b952ed63
dmoos <dmoos@datarecognitioncorp.com>
2012-12-21 12:27:10 -0600

    remove comments

--- src/com/dave/GeneralTest.java
+++ src/com/dave/GeneralTest.java
@@ -8,8 +8,6 @@ public class GeneralTest {
 	public static void main(String[] args) {
 		System.out.println("Say Hello localedit");
 		System.out.println("Local edit conflict with cloud edit");
-		//cloud edit
-		// cloud #2
 		
 	}
 }
