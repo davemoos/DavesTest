3cd37b0bcce1d7cc82a13d294a197745669cc720
dmoos <dmoos@datarecognitioncorp.com>
2012-12-21 12:31:01 -0600

    asdfa

--- src/com/dave/GeneralTest.java
+++ src/com/dave/GeneralTest.java
@@ -9,6 +9,7 @@ public class GeneralTest {
 		System.out.println("Say Hello localedit");
 		System.out.println("Local edit conflict with cloud edit");
 		// cloud added #2
-		
+		// local added #2
+		// cloud added #3
 	}
 }
