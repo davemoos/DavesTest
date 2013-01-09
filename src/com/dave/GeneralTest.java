a074e0483c6b1059d883b72d643bd1099cae17e8
dave <dmoos@datarecognitioncorp.com>
2013-01-09 11:22:55 -0600

    local edit 1/1/2013

--- src/com/dave/GeneralTest.java
+++ src/com/dave/GeneralTest.java
@@ -6,8 +6,7 @@ public class GeneralTest {
      * @param args
      */
     public static void main(String[] args) {
-        System.out.println("Say Hello localedit");
-        System.out.println("Local edit conflict with cloud edit");
+        System.out.println("Say Hello localedit");     
     }
 
 
