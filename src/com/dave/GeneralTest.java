8e42aff10e74a12e44d208c536f070db40df4a7d
dave <dmoos@datarecognitioncorp.com>
2013-03-18 08:34:13 -0500

    test

--- src/com/dave/GeneralTest.java
+++ src/com/dave/GeneralTest.java
@@ -8,7 +8,8 @@ public class GeneralTest {
     public static void main(String[] args) {
         //local edit  
         //remote edit
-        // davebranch1 edit
+        // davebranch1 edit  
+        
     }
 
 
