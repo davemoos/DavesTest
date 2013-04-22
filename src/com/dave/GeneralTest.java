ec89f074a2b07051853029a2e024d2d04da9c5d2
dave <dmoos@datarecognitioncorp.com>
2013-04-22 13:10:42 -0500

    aa

--- src/com/dave/GeneralTest.java
+++ src/com/dave/GeneralTest.java
@@ -6,8 +6,7 @@ public class GeneralTest {
      * @param args 
      */
     public static void main(String[] args) {
-        // just 1 remote
-        // just2 local
+        // dave local edit
     }
 
 
