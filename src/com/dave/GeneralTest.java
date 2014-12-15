e8d69de7f63d9154fae365ff3b1f66a0b6bdaa48
David Moos <davemoos@gmail.com>
2014-12-15 12:42:37 -0600

    Update GeneralTest.java
    dfasd

--- src/com/dave/GeneralTest.java
+++ src/com/dave/GeneralTest.java
@@ -6,10 +6,7 @@ public class GeneralTest {
      * @param args 
      */
     public static void main(String[] args) {
-	    System.out.println("Hello World");
-	    System.out.println("Hello World2 - Cloud edit");
-	    System.out.println("Hello World - local edit");
-	    //dave
+	    //remote master changes.
     }
 
 
